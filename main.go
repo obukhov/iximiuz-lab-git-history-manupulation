1300d92e921c3e20963281c9a02a4164b4e48d2a
Perfect Coder <perfect.coder@example.com>
2024-01-23 23:28:23 +0100

    Add /signByDate endpoint

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/signs", signsHandler)
+	http.HandleFunc("/signByDate", signByDateHandler)
 
 	fmt.Println("Server listening on :8080")
 	err := http.ListenAndServe(":8080", nil)
@@ -34,4 +36,51 @@ func signsHandler(w http.ResponseWriter, r *http.Request) {
 	})
 
 	w.Write(b)
+}
+
+func signByDateHandler(w http.ResponseWriter, r *http.Request) {
+	dateString := r.URL.Query().Get("date")
+	if dateString == "" {
+		http.Error(w, "Date parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	date, err := time.Parse("2006-01-02", dateString)
+	if err != nil {
+		http.Error(w, "Invalid date format. Use YYYY-MM-DD", http.StatusBadRequest)
+		return
+	}
+
+	fDate := date.Format("01-02")
+	sign := "Unknown"
+
+	for _, s := range []struct{ till, sign string }{
+		{"01-19", "Capricorn"},
+		{"02-18", "Aquarius"},
+		{"03-21", "Pisces"},
+		{"04-19", "Aries"},
+		{"05-20", "Taurus"},
+		{"06-20", "Gemini"},
+		{"07-22", "Cancer"},
+		{"08-22", "Leo"},
+		{"09-22", "Virgo"},
+		{"10-22", "Libra"},
+		{"11-21", "Scorpio"},
+		{"12-21", "Sagittarius"},
+		{"12-31", "Capricorn"},
+	} {
+		if fDate <= s.till {
+			sign = s.sign
+			break
+		}
+	}
+
+	jsonData, err := json.Marshal(sign)
+	if err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
