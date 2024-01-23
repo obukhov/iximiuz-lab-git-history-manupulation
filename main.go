a48d2cffea4d4a3b08960297fa8a2afef8f37c20
Perfect Coder <perfect.coder@example.com>
2024-01-23 22:48:14 +0100

    Add endpoint

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/signs", signsHandler)
+
 	fmt.Println("Server listening on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
+}
+
+func signsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	b, _ := json.Marshal([]string{
+		"Aries",
+		"Taurus",
+		"Gemini",
+		"Cancer",
+		"Leo",
+		"Virgo",
+		"Libra",
+		"Scorpio",
+		"Sagittarius",
+		"Capricorn",
+		"Aquarius",
+		"Pisces",
+	})
+
+	w.Write(b)
 }
