b86f176054f660e37932d8ceb69f1a277181e7f3
Perfect Coder <perfect.coder@example.com>
2024-01-23 22:40:46 +0100

    Add web server

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Server listening on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
